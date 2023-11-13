f48461290b2ab59a98fdbbad08f93130e5472a4e
nadiraaraa <nadiraaraa@gmail.com>
2023-11-13 13:06:48 +0700

    feat/landing-fin-for-now

--- src/app/landing/components/NavBar.tsx
+++ src/app/landing/components/NavBar.tsx
@@ -4,31 +4,31 @@ export default function NavBar() {
     <>
         {/* <div className="flex flex-row h-screen w-40 md:h-20 md:w-screen md:flex-col md:overflow-hidden bg-[#1C2434]"> */}
 
-        <nav className="bg-[#1C2434] border-gray-200">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <nav className="bg-[#1C2434] border-gray-200 px-10">
+            <div className="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 md: p-0">
+                <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="images/logo_img.png" className="h-8" alt="Flowbite Logo" />
+                    <img src="images/logo_title.png" className="h-8" alt="Flowbite Logo" />
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10  text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border text-white">
+                    <ul className="font-lg flex flex-col md:flex-row p-4 md:p-0 md:m-0 text-white">
                         <li>
-                        <a href="#" className="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 " aria-current="page">Home</a>
+                        <a href="#" className="text-2xl block text-white hover:bg-gradient-to-b from-[#C2E799] to-[#4D5D52]  p-6">Home</a>
+                        </li>
+                        <li >
+                        <a href="#" className="text-2xl block text-white hover:bg-gradient-to-b from-[#C2E799] to-[#4D5D52]  p-6">Why Us</a>
                         </li>
                         <li>
-                        <a href="#" className="block py-2 px-3 text-white rounded hover:bg-[#C2E799]-70 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Why Us</a>
+                        <a href="#" className="text-2xl block text-white hover:bg-gradient-to-b from-[#C2E799] to-[#4D5D52]  p-6">Our Packages</a>
                         </li>
                         <li>
-                        <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Our Packages</a>
-                        </li>
-                        <li>
-                        <a href="#" className="block py-2 px-3 text-[#C2E799] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</a>
+                        <a href="#" className="text-2xl block text-white hover:bg-gradient-to-b from-[#C2E799] to-[#4D5D52]  p-6 hover:text-white">Register</a>
                         </li>
                     </ul>
                 </div>
