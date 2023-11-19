70ad25a5c9bb398c992a5ecff391e98fa867dd74
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2023-11-19 19:41:08 +0700

    authorization middleware
    create middleware for authorization API

---
+++ src/app/api/authorized.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+// Middleware function to verify JWT token
+async function authorized(req: Request) {
+  try {
+    // Extract the token from the Authorization header
+    const authHeader = req.headers.get("Authorization");
+    if (!authHeader) {
+      return { message: "Authorization header missing", status: 401 };
+    }
+
+    const token = authHeader.split(" ")[1]; // Assumes "Bearer TOKEN"
+    if (!token) {
+      return { message: "Token missing", status: 401 };
+    }
+
+    // Verify the JWT token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    if (!decoded) {
+      return { message: "Invalid token", status: 401 };
+    }
+
+    return { message: "Token is valid", status: 200 };
+  } catch (error) {
+    return { message: "Invalid token", status: 401 };
+  }
+}
+
+export default authorized;
