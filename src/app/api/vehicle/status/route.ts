1d33e0af9da83bba76b688688b178c9366295f36
benyaminjodi <18221147@std.stei.itb.ac.id>
2023-11-21 19:19:34 +0700

    be vehicle status

---
+++ src/app/api/vehicle/status/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import type { Vehicle } from "@prisma/client";
+import authorized from "../../authorized";
+const prisma = new PrismaClient();
+
+export const GET = async (request: Request) => {
+  const auth = await authorized(request);
+  if (auth.status !== 200) {
+    return NextResponse.json(auth, { status: auth.status });
+  }
+  const vehicles = await prisma.vehicle.findMany({
+    orderBy: {
+      id: "asc",
+    },
+    select:{
+        id: true,
+        plate : true,
+        status : true
+      }
+  });
+  return NextResponse.json (vehicles);
+};
