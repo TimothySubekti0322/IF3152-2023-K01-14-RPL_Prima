1b5aced0b64aee1b230ed47de9959fd46edc4ef3
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2023-11-22 16:25:25 +0700

    Database Seeder
    Adding Database Seeder

---
+++ prisma/seed/instructorData.ts
@@ -0,0 +1,35 @@
+// Instructor Data (seed)
+const instructorData = [
+  {
+    name: "Nadira",
+    nik: "5171042404720006",
+    address: "Jl. Cipadung no. 9",
+    phone: "081133557799",
+  },
+  {
+    name: "Timothy",
+    nik: "5171042405160007",
+    address: "Jl. Kramat Jaya Baru E9",
+    phone: "084789266182",
+  },
+  {
+    name: "Nadine",
+    nik: "5171042409870008",
+    address: "Jl. Ciumbeulit no 14",
+    phone: "088902635512",
+  },
+  {
+    name: "Carrisa",
+    nik: "5171042404560009",
+    address: "Jl. Tubagus Ismail no 5",
+    phone: "089477364819",
+  },
+  {
+    name: "Jodi",
+    nik: "5171042404230002",
+    address: "Jl. Menteng Agung no 8",
+    phone: "0826615444",
+  },
+];
+
+export default instructorData;
---
+++ prisma/seed/userData.ts
@@ -0,0 +1,19 @@
+// User Data (seed)
+const userData = [
+  {
+    email: "owner@gmail.com",
+    password: "owner",
+    phone: "081234567891",
+    location: "Jakarta",
+    role: "Owner",
+  },
+  {
+    email: "admin@gmail.com",
+    password: "admin",
+    phone: "089988776655",
+    location: "Bandung",
+    role: "Admin",
+  },
+];
+
+export default userData;
