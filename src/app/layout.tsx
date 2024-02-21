33720957526be1a8f5f78d3159b965ce65df7424
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2024-02-21 20:44:51 +0700

    redeploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,25 @@
 import type { Metadata } from "next";
-import { Inter , Open_Sans} from "next/font/google";
+import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
-import Head from "./head"
+import Head from "./head";
 
 const inter = Inter({ subsets: ["latin"] });
 const openSans = Open_Sans({ subsets: ["latin-ext"] });
 
 export const metadata: Metadata = {
-  title: 'RPL Prima',
-  description: 'Selamat datang di Kursus Mengemudi RPL Prima',
-}
+  title: "RPL Prima",
+  description: "Selamat datang di Kursus Mengemudi RPL Prima",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (      
-      <html lang="en">
-          <Head/>
-          <body className={`${openSans.className}`}>{children}</body>
-      </html>
-    
+  return (
+    <html lang="en">
+      <Head />
+      <body className={`${openSans.className}`}>{children}</body>
+    </html>
   );
 }
-
